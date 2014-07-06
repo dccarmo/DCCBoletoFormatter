d68af39dfe6e0202f8958cf236a5f7e5364ca74a
Diogo do Carmo <diogo@dcarmo.me>
2014-07-06 20:07:14 -0300

    First commit

---
+++ DCCBoletoFormatter.h
@@ -0,0 +1,15 @@
+//
+//  DCCBoletoFormatter.h
+//
+//  Created by Diogo do Carmo on 16/05/14.
+//  Copyright (c) 2014 Diogo do Carmo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DCCBoletoFormatter : NSFormatter
+
+- (NSString *)linhaDigitavelDoCodigoBarra:(NSString *)codigoBarra;
+- (NSString *)codigoBarraDaLinhaDigitavel:(NSString *)linhaDigitavel;
+
+@end
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-DCCBoletoFormatter
-==================
+###DCCBoletoFormatter
+===================
+
+####Sobre
+
+Um formatador que converte o código de barras de um boleto em uma linha digitável, e vice-versa.
